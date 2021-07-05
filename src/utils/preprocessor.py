d6f19310270fd5d107db333439a0b942c8b41d66
sgourav <sgourav@vmware.com>
2021-07-05 14:58:58 +0530

    Replaced the deprecated library with new one and made necessary changes where it was being used

--- src/utils/preprocessor.py
+++ src/utils/preprocessor.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.misc import imread, imresize
+from imageio import imread
+from PIL import Image
 
 
 def preprocess_input(x, v2=True):
@@ -16,6 +17,7 @@ def _imread(image_name):
 
 
 def _imresize(image_array, size):
+        resized_image = numpy.array(Image.fromarray(image_array).resize(size))
         return imresize(image_array, size)
 
 
